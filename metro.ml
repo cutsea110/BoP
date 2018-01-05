ee1e91d75b0ad768b9711e7415d3409c9831930e
cutsea110 <cutsea110@gmail.com>
2018-01-05 13:05:53 +0900

    define ekimei_t and hyoji

--- metro.ml
+++ metro.ml
@@ -1,1 +1,10 @@
 (* メトロネットワーク最短路問題 *)
+(* 駅名 *)
+type ekimei_t =
+{ kanji  : string;
+  kana   : string;
+  romaji : string;
+  shozoku : string;
+}
+let hyoji eki = match eki with
+    { kanji = n; kana = k; romaji = e; shozoku = l } -> l ^ " " ^ n ^ " (" ^ k ^ ")"
